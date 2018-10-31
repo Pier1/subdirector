5d443dedfa5b6a274e48600b0ef2f28a3f92d4f0
Kendrick Arnett <kmarnett@gmail.com>
2018-10-31 15:28:07 -0500

    Hash out demo & watch tasks.

--- gulp-tasks/watch.js
+++ gulp-tasks/watch.js
@@ -1,12 +1,18 @@
 'use strict';
 (() => {
   module.exports = (gulp, plugins, config) => {
+    const browserSync = require('browser-sync').create();
     return () => {
+      browserSync.init({
+        server: {
+            baseDir: `./demo/`
+        }
+      });
+
       gulp.watch([`${config.srcPath}/*.scss`, `!${config.srcPath}/demo.scss`], ['build']);
       gulp.watch([`${config.srcPath}/*.pug`, `!${config.srcPath}/demo.pug`], ['build']);
       gulp.watch([`${config.srcPath}/*.js`], ['build']);
-      gulp.watch(`${config.buildPath}/build/demo.html`)
-        .on('change', plugins.browserSync.reload);
+      gulp.watch([`${config.srcPath}/demo/**/*`], ['demo']).on('change', browserSync.reload);
 
       return true;
     }
